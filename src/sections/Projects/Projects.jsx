ce1e5201ffbddc1b0deb2cb0f788f01dbc1a93dd
ryuselmi <yuselmirodriguez24@gmail.com>
2024-06-27 17:57:05 -0700

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -11,19 +11,19 @@ function Project() {
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={numbersFloating}
-          link="https://github.com/ryuselmi/tictactoe"
+          link="https://yuselmi-numberguessinggame.netlify.app/"
           h3="Tic-Tac-Toe"
           p="Two-Player Game"
         />
         <ProjectCard
           src={ticBoard}
-          link="https://github.com/ryuselmi/Number-Guessing-Game"
+          link="https://yuselmitictactoe.netlify.app/"
           h3="Number Guessing Game"
           p="Guess the number in less than 10 tries!"
         />
         <ProjectCard
           src={calcPicture}
-          link="https://github.com/ryuselmi/calculatoryr"
+          link="https://yuselmi-calculator.netlify.app/"
           h3="Calculator"
           p="A working, functional calculator"
         />
